2d44ed8b456345379a7af47d4e4d9bbcba29d46c
Boombambap1 <stevenw000098@gmail.com>
2025-09-30 23:08:53 -0700

    Base of geostate and gamestatedone

--- Assets/Scripts/Model/GameState.cs
+++ Assets/Scripts/Model/GameState.cs
@@ -1,18 +1,69 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GameState : MonoBehaviour
+ public class GameState
 {
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<Vector3Int, GameObjectData> objects;
+
+    public GameState()
     {
-        
+        objects = new Dictionary<Vector3Int, GameObjectData>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaceObjectAt(GameObjectData obj, Vector3Int pos)
     {
-        
+        objects[pos] = obj;
+    }
+
+    public void RemoveObjectAt(Vector3Int pos)
+    {
+        objects.Remove(pos);
+    }
+
+    public void MoveObjectTo(GameObjectData obj, Vector3Int newPos)
+    {
+        Vector3Int oldPos = obj.Position;
+        objects.Remove(oldPos);
+        obj.Position = newPos;
+        objects[newPos] = obj;
+    }
+
+    public void ClearAllObjects()
+    {
+        objects.Clear();
+    }
+
+    public GameObjectData GetObjectAt(Vector3Int pos)
+    {
+        objects.TryGetValue(pos, out GameObjectData obj);
+        return obj;
+    }
+
+    public Vector3Int GetObjectPos(GameObjectData obj)
+    {
+        return obj.Position;
+    }
+
+    public bool IsWinningState()
+    {
+        // You can fill this in later
+        return false;
     }
 }
+
+public class GameObjectData
+{
+    public Vector3Int Position;
+    public string Color;
+    public bool IsAlive;
+    public bool InFreefall;
+
+    public GameObjectData(Vector3Int pos, string color)
+    {
+        Position = pos;
+        Color = color;
+        IsAlive = true;
+        InFreefall = false;
+    }
+}
+
+
--- Assets/Scripts/Model/GeoState.cs
+++ Assets/Scripts/Model/GeoState.cs
@@ -2,18 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GeoState : MonoBehaviour
+public class GeoState
 {
-    public bool isGeo;
-    // Start is called before the first frame update
-    void Start()
+    private Dictionary<Vector3Int, GeoType> geoMap;
+
+    public GeoState()
     {
-        
+        geoMap = new Dictionary<Vector3Int, GeoType>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaceGeoAt(Vector3Int pos, GeoType type)
     {
-        
+        geoMap[pos] = type;
+    }
+
+    public void RemoveGeoAt(Vector3Int pos)
+    {
+        geoMap.Remove(pos);
+    }
+
+    public void ClearAllGeo()
+    {
+        geoMap.Clear();
+    }
+
+    public bool CheckVoidAt(Vector3Int pos)
+    {
+        return !geoMap.ContainsKey(pos);
+    }
+
+    public GeoType GetGeoTypeAt(Vector3Int pos)
+    {
+        return geoMap.ContainsKey(pos) ? geoMap[pos] : GeoType.None;
+    }
+
+    public bool CheckGeoTypeAt(Vector3Int pos, GeoType type)
+    {
+        return geoMap.TryGetValue(pos, out GeoType existingType) && existingType == type;
     }
 }
+
+
